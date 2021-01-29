bd03be496b8f846f0bd97c4572ae4c0c17437891
Kenneth Coffie <geniounico@outlook.com>
2021-01-29 22:14:05 +0000

    fixing colormode stuff

--- shared/utils/theme.ts
+++ shared/utils/theme.ts
@@ -1,11 +1,13 @@
-import theme from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
 
-const customTheme = {
-  config: {
-    initialColorMode: "light", // "light" | "dark"
-  },
-  ...theme,
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+}
 
-};
+const customTheme = extendTheme({
+  config,
+
+});
 
 export default customTheme
