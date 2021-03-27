2f3ee3821962540a169412ed1eac3099206e5b00
Kenneth Coffie <geniounico@outlook.com>
2021-03-27 23:10:55 +0000

    fix minor

--- shared/hooks/useAuth.ts
+++ shared/hooks/useAuth.ts
@@ -1,12 +1,12 @@
 
 import { useStoreActions, useStoreState } from "easy-peasy"
-import { iAuth } from "../store/auth"
 
 
 
 
 
-function useAuth(): iAuth {
+
+function useAuth() {
   const auth = useStoreState(state => state.auth)
   const setToken = useStoreActions<any, any>(state => state.auth.setToken)
   const setUser = useStoreActions<any, any>(state => state.auth.setUser)
