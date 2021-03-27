50c2d2427ab3c3bfe02817915345ccff7e7c5b4b
Kenneth Coffie <geniounico@outlook.com>
2021-03-27 22:41:46 +0000

    included css for mapbox

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { StoreProvider } from 'easy-peasy';
 import "../css/index.css";
 import store from "../shared/store";
 import theme from '../shared/utils/theme';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 
 function MyApp({ Component, pageProps }) {
